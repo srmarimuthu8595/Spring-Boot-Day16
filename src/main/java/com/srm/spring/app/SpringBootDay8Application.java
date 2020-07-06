140f4b0c72636074ef7f6451f0d05d599e7d1b95
unknown <srmarimuthu6666@gmail.com>
2020-07-06 23:16:40 +0530

    first commit

---
+++ src/main/java/com/srm/spring/app/SpringBootDay8Application.java
@@ -0,0 +1,13 @@
+package com.srm.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay8Application.class, args);
+	}
+
+}
