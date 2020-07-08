ffa628cdc8e28183ed6deab18b894d8d913d500e
unknown <srmarimuthu6666@gmail.com>
2020-07-08 20:20:42 +0530

    Mytodaychanges

--- src/main/java/com/srm/spring/app/SpringBootDay8Application.java
+++ src/main/java/com/srm/spring/app/SpringBootDay8Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDay8Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootDay8Application.class, args);
+	
 	}
 
 }
